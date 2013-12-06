0d29996f215a708f458439cf5b86a8deef693b39
Pablo Colapinto <wolftype@gmail.com>
2013-12-06 12:45:36 -0800

    cubicLattice

---
+++ src/vsr_cga3D_cubicLattice.cpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  vsr_cga3D_cubicLattice.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/03/2013 12:51:56
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Pablo Colapinto (), wolftype (gmail)
+ *   Organization:  Media Arts and Technology Program, UCSB
+ *
+ * =====================================================================================
+ */
+#include "vsr_cubicLattice.h"
+
+namespace vsr{
+
+    //  template class CubicLattice< NPnt<5> >;
+
+      template<>
+      void CubicLattice< NPnt<5> > :: initPoints(){
+         for (int i = 0; i < mWidth; ++i){
+            for (int j = 0; j < mHeight; ++j){
+                for (int k = 0; k < mDepth; ++k){
+                    int tidx = idx(i,j,k); 
+                    mPoint[ tidx ]  = Ro::null( px(i),  py(j),  pz(k) ); 
+                }
+            }
+         }
+      }
+
+      template class CubicLattice< NPnt<5> >;
+
+
+}
+
