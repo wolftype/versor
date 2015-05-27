c123b82a945dad021c1bc725158ca17177d7891e
wolftype <wolftype@gmail.com>
2015-05-27 15:23:06 -0700

    versor group

--- vsr/form/vsr_group.h
+++ vsr/form/vsr_group.h
@@ -579,52 +579,52 @@ struct SpaceGroup3D : PointGroup3D<V> {
     void init(){
 
 
-        //GLIDE replacements for non-symmorphic groups
-        vector<int> replace; //keep track of indices into this->ops to replace
-        vector<V> nops;     //new operations list
-        switch(mGlide.a.type){
-          case AxialB: //< replace a reflection with a glide
-          {
-            replace.push_back(0);
-            auto trs = gen::trs( (mGlide.a.bInvert ? (this->a - this->b) : this->b ) * .5 );
-            this->gops.push_back( this->a * trs );
-            break;
-          }
-          case AxialC:
-          {
-            replace.push_back(0);
-            auto trs = gen::trs( (mGlide.a.bInvert ? (this->a - this->c) : this->c ) * .5 );
-            this->gops.push_back( this->a * trs );
-            break;
-          }
-          case Diagonal:
-          {
-            replace.push_back(0); //we will replace 0 idx
-            auto trs = gen::trs(  (mGlide.a.bInvert ? (this->a - (this->b+this->c) : this->b+this->c ) * .5 );
-            this->gops.push_back( this->a * trs ); 
-            break;
-          }
-          
-        }
+ //       //GLIDE replacements for non-symmorphic groups
+ //       vector<int> replace; //keep track of indices into this->ops to replace
+ //       vector<V> nops;     //new operations list
+ //       switch(mGlide.a.type){
+ //         case AxialB: //< replace a reflection with a glide
+ //         {
+ //           replace.push_back(0);
+ //           auto trs = gen::trs( ( (mGlide.a.bInvert) ? (this->a - this->b) : this->b ) * .5 );
+ //           this->gops.push_back( this->a * trs );
+ //           break;
+ //         }
+ //         case AxialC:
+ //         {
+ //           replace.push_back(0);
+ //           auto trs = gen::trs( ( (mGlide.a.bInvert) ? (this->a - this->c) : this->c ) * .5 );
+ //           this->gops.push_back( this->a * trs );
+ //           break;
+ //         }
+ //         case Diagonal:
+ //         {
+ //           replace.push_back(0); //we will replace 0 idx
+ //           auto trs = gen::trs( ( (mGlide.a.bInvert) ? ( (this->a - (this->b+this->c)) : this->b+this->c ) * .5 );
+ //           this->gops.push_back( this->a * trs ); 
+ //           break;
+ //         }
+ //         
+ //       }
 
-        switch(mGlide.b.type){
+ //       switch(mGlide.b.type){
 
-        }
+ //       }
 
-        switch(mGlide.c.type){
+ //       switch(mGlide.c.type){
 
-        }
+ //       }
 
-        for (int i = 0; i < this->ops.size(); ++i){
-          bool add=true;
-          for (auto& j : replace) if (i==j) add = false;
-          if (bool) nops.push_back(i);
-        }
+ //       for (int i = 0; i < this->ops.size(); ++i){
+ //         bool add=true;
+ //         for (auto& j : replace) if (i==j) add = false;
+ //         if (bool) nops.push_back(i);
+ //       }
 
-        //SCREW replacements
+ //       //SCREW replacements
 
-        //Reseed opIdx
-        seed();
+ //       //Reseed opIdx
+ //       seed();
 
     }
     
